e3773097c236f85d28533ee054034fa4c9b2d3f6
ria-choi <maria456852@gmail.com>
2023-11-23 11:06:57 +0900

    commit

---
+++ src/main/main.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+    "context"
+    "log"
+    "net"
+	"fmt"
+    "time"
+	"os"
+
+    // grpc
+    pb "opencsd/src/config"
+    "google.golang.org/grpc"
+
+    // influxdb v1 client
+    client "github.com/influxdata/influxdb/client/v2"
+)
+
+var(
+	//INSTANCE_METRIC_COLLECTOR_IP = "10.0.4.80"
+	//INSTANCE_METRIC_COLLECTOR_PORT = "40802"
+
+	// INFLUXDB_IP = "10.0.4.87"
+	// INFLUXDB_PORT = "40700"
+	
+    INFLUXDB_CSD_DB = "OpenCSD_Management_Platform"
+    INFLUXDB_CSD_MEASUREMENT = "csd_metric"
+
+    INFLUXDB_ETCD_DB = "OpenCSD_Management_Platform"
+    INFLUXDB_ETCD_MEASUREMENT = "etcd_metric"
+)
+
+type server struct {
+    pb.UnimplementedCSDMetricServer // csd metric method stub
+	pb.UnimplementedEtcdMetricServer // etcd metric method stub
+}
+
+type CSDMetricData struct {
+	ID			 int32	
+	CPUUSAGE 	 float64
+	MEMUSAGE	 float64
+	NETWORKUSAGE float64
+}
+
+type EtcdMetricData struct {
+
+}
+
+// CSD stub handler server
+func (s *server) ReceiveCSDMetric(ctx context.Context, in *pb.CSDMetricRequest) (*pb.MetricResponse, error) {
+	// request data parsing
+	id := in.GetId()
+    cpuUsage := in.GetCpuUsage()
+    memUsage := in.GetMemUsage()
+    networkSpeed := in.GetNetworkSpeed()
+	
+    // 수신 데이터 로그 출력
+    log.Printf("Received data: Id=%d, CpuUsage=%.4f, MemUsage=%.4f, NetworkSpeed=%.4f", id, cpuUsage, memUsage, networkSpeed)
+
+	// 메트릭 구조체 생성 및 초기화
+	var csdMetricData CSDMetricData
+	csdMetricData.ID = id
+	csdMetricData.CPUUSAGE = cpuUsage
+	csdMetricData.MEMUSAGE = memUsage
+	csdMetricData.NETWORKUSAGE = networkSpeed
+
+    // csd metric DB에 삽입
+    CSDMetricInsert(&csdMetricData)
+	
+    // // Instance Metric Collector에 metric 전달
+	// CSDMetricSender(INSTANCE_METRIC_COLLECTOR_IP, INSTANCE_METRIC_COLLECTOR_PORT, &csdMetricData)
+
+	// client stub에게 response
+	return &pb.MetricResponse{JsonConfig: `request success`}, nil
+}
+
+func CSDMetricInsert(metricData *CSDMetricData) {
+	INFLUX_IP := os.Getenv("INFLUX_IP")
+    INFLUX_PORT := os.Getenv("INFLUX_PORT")
+    INFLUX_USERNAME := os.Getenv("INFLUX_USERNAME")
+    INFLUX_PASSWORD := os.Getenv("INFLUX_PASSWORD")
+
+	c, err := client.NewHTTPClient(client.HTTPConfig{ // InfluxDB 연결
+		Addr: "http://" + INFLUX_IP + ":" + INFLUX_PORT, 
+		Username: INFLUX_USERNAME,
+		Password: INFLUX_PASSWORD,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{ // 배치 포인트 생성
+		Database:  INFLUXDB_CSD_DB, // DB 이름
+ 		Precision: "s",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// tags := map[string]string{"flags": "csd-metric"} //etcd 정보인지 csd 정보인지 분기 (etcd : 1, csd metric : 2)
+	fields := map[string]interface{}{
+		"currTime": time.Now().Format("15:04:05"),
+		"id": metricData.ID,
+		"cpuUsage":  metricData.CPUUSAGE,
+		"memeUsage": metricData.MEMUSAGE,
+		"networkUsage": metricData.NETWORKUSAGE,
+	}
+
+	pt, err := client.NewPoint(INFLUXDB_CSD_MEASUREMENT, nil, fields, time.Now()) //measurements에 tag, field, time insert => 필요 없는 값이면 nil로 설정, time은 nil 설정이 안됨
+	if err != nil {
+		log.Fatal(err)
+	}
+	bp.AddPoint(pt) // 생성해둔 배치 포인트에 새로운 데이터 추가 
+
+	err = c.Write(bp) //influxdb에 write
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// q := client.Query{ // 쿼리 날려서 추가한 데이터 확인
+	// 	Command:  "SELECT * FROM csd_metric",
+	// 	Database: "test",
+	// }
+	// if response, err := c.Query(q); err == nil && response.Error() == nil {
+	// 	fmt.Println(response.Results)
+	// }
+}
+
+// //80서버 instance metric collector grpc 서버 접속 및 메트릭 전송
+// func CSDMetricSender(ip string, port string, metricData *CSDMetricData) {
+//     // gRPC 서버에 연결
+//     conn, err := grpc.Dial(ip + ":" + port, grpc.WithInsecure())
+//     if err != nil {
+//         log.Fatalf("Could not connect: %v", err)
+//     }
+//     defer conn.Close()
+
+//     // gRPC 클라이언트 생성
+//     client := pb.NewCSDMetricClient(conn)
+
+// 	// gRPC 메서드 호출 => csd metric 기반 request 생성 및 전송 후 response 수신
+// 	request := &pb.CSDMetricRequest{Id : metricData.ID, CpuUsage : metricData.CPUUSAGE, MemUsage : metricData.MEMUSAGE, NetworkSpeed: metricData.NETWORKUSAGE}
+    
+// 	// grpc 서버 응답
+// 	response, err := client.ReceiveCSDMetric(context.Background(), request)
+//     if err != nil {
+//         log.Fatalf("Get Response Error From Grpc Server: %v", err)
+//     }
+//     fmt.Printf("Response From gRPC Server: %s\n", response.JsonConfig) //응답 형식 확인해야 함
+// }
+
+// etcd stub handler server
+// func (s *server) Get(ctx context.Context, in *pb.EtcdMetricRequest) (*pb.EtcdMetricResponse, error) {
+// 	// request data parsing
+
+// 	// 수신 데이터 로그 출력
+
+// 	// etcd metric DB에 삽입
+// 	etcdMetricInsert()
+
+// 	// client stub에게 response
+// 	return &pb.EtcdMetricResponse{JsonConfig: `request success`}, nil
+// }
+
+// func etcdInsert(){
+
+// }
+
+func main() {
+    lis, err := net.Listen("tcp", ":40801")
+	//fmt.Println("grpc server created")
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+    
+    // grpc 서버 생성
+    s := grpc.NewServer()
+
+	// stub handler 서버 등록 
+	pb.RegisterCSDMetricServer(s, &server{}) // register csd metric server
+	// pb.RegisterEtcdMetricServer(s, &server{}) // register etcd metric server start
+    
+	fmt.Println("gRPC Server Created [port : 40801]")
+	
+	if err := s.Serve(lis); err != nil {
+        log.Fatalf("failed to serve: %v", err)
+    }
+}
