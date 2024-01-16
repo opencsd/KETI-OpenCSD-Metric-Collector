bfc6dd0a7c9a544c8bd2413840ece5809de3a30f
ria-choi <wngus0331@gmail.com>
2024-01-16 17:47:40 +0900

    db name update

--- src/main/main.go
+++ src/main/main.go
@@ -8,7 +8,6 @@ import (
     "time"
 	"os"
 	"strconv"
-
     // grpc
     pb "opencsd/src/config"
     "google.golang.org/grpc"
@@ -25,12 +24,17 @@ var(
     // INFLUXDB_CSD_MEASUREMENT = "csd_metric"
 
     INFLUXDB_ETCD_DB = "opencsd_management_platform"
-    INFLUXDB_ETCD_MEASUREMENT = "etcd_metric"
+    // INFLUXDB_ETCD_MEASUREMENT = "etcd_metric"
+
+	INFLUXDB_NODE_DB = "opencsd_management_platform"
+
 )
 
 type server struct {
     pb.UnimplementedCSDMetricServer // csd metric method stub
+	pb.UnimplementedNodeMetricServer // node metric method stub
 	// pb.UnimplementedEtcdMetricServer // etcd metric method stub
+
 }
 
 type CSDMetricData struct {
@@ -53,11 +57,79 @@ type CSDMetricData struct {
 	NetworkTxData	  int32     `json:"networkTxData"`
 
 	CsdMetricScore    float64 	`json:"csdMetricScore"`
-	CsdMetricGrade    string
+	CsdMetricGrade    string	
+}
+
+type NodeMetricData struct {
+	Id				     int64
+	
+	NodeIp				 string
+	NodeName			 string
+	NodeStatus			 string
+
+	TotalCpuCapacity	 int64	 
+	CpuUsage			 int64
+
+	TotalMemCapacity	 int64
+	MemUsage			 int64
+
+	TotalStorageCapacity int64
+	StorageUsage		 int64
+
+	NetworkRxData		 int64
+	NetworkTxData		 int64
 }
 
 type EtcdMetricData struct {
 	ID				int32
+}
+
+// Node stub handler server
+func (s *server) ReceiveNodeMetric(ctx context.Context, in *pb.NodeMetricRequest) (*pb.MetricResponse, error) {
+	id := in.GetId()
+
+	nodeName := in.GetNodeName()
+	nodeIp := in.GetNodeIp()
+	nodeStatus := in.GetNodeStatus()
+
+	totalCpuCapacity := in.GetTotalCpuCapacity()
+    cpuUsage := in.GetCpuUsage()
+
+	totalMemCapacity := in.GetTotalMemCapacity()
+    memUsage := in.GetMemUsage()
+
+	totalStorageCapacity := in.GetTotalDiskCapacity()
+    storageUsage := in.GetDiskUsage()
+
+	networkRxData := in.GetNetworkRxData()
+	networkTxData := in.GetNetworkTxData()
+
+	//  receive log
+
+	var nodeMetricData NodeMetricData
+
+	nodeMetricData.Id = id
+	
+	nodeMetricData.NodeIp = nodeIp
+	nodeMetricData.NodeName = nodeName
+	nodeMetricData.NodeStatus = nodeStatus
+
+	nodeMetricData.TotalCpuCapacity = totalCpuCapacity
+	nodeMetricData.CpuUsage = cpuUsage
+
+	nodeMetricData.TotalMemCapacity = totalMemCapacity
+	nodeMetricData.MemUsage = memUsage
+
+	nodeMetricData.TotalStorageCapacity = totalStorageCapacity
+	nodeMetricData.StorageUsage = storageUsage
+
+	nodeMetricData.NetworkRxData = networkRxData
+	nodeMetricData.NetworkTxData = networkTxData
+	fmt.Printf("%+v\n", nodeMetricData)
+	nodeMetricInsert(&nodeMetricData)
+
+	// client stub에게 response
+	return &pb.MetricResponse{JsonConfig: `request success`}, nil
 }
 
 // CSD stub handler server
@@ -95,6 +167,7 @@ func (s *server) ReceiveCSDMetric(ctx context.Context, in *pb.CSDMetricRequest) 
 
 	// 메트릭 구조체 생성 및 초기화
 	var csdMetricData CSDMetricData
+	
 	csdMetricData.Id = id
 
 	csdMetricData.TotalCpuCapacity = totalCpuCapacity
@@ -150,7 +223,7 @@ func CSDMetricInsert(metricData *CSDMetricData) {
 
 	//INSTANCE_METRIC_COLLECTOR_IP = "10.0.4.80"
 	//INSTANCE_METRIC_COLLECTOR_PORT = "40802"
-
+	
 	c, err := client.NewHTTPClient(client.HTTPConfig{ // InfluxDB 연결
 		Addr: "http://" + INFLUX_IP + ":" + INFLUX_PORT, 
 		Username: INFLUX_USERNAME,
@@ -208,13 +281,68 @@ func CSDMetricInsert(metricData *CSDMetricData) {
 		log.Fatal(err)
 	}
 
-	// q := client.Query{ // 쿼리 날려서 추가한 데이터 확인
-	// 	Command:  "SELECT * FROM csd1_metric LIMIT 10",
-	// 	Database: INFLUXDB_CSD_DB,
-	// }
-	// if response, err := c.Query(q); err == nil && response.Error() == nil {
-	// 	fmt.Println(response.Results)
-	// }
+}
+
+// IP, Port, username, password 
+
+func nodeMetricInsert(metricData *NodeMetricData){
+	INFLUX_IP := os.Getenv("INFLUX_IP") 
+	INFLUX_PORT := os.Getenv("INFLUX_PORT")
+    INFLUX_USERNAME := os.Getenv("INFLUX_USERNAME")
+    INFLUX_PASSWORD := os.Getenv("INFLUX_PASSWORD")
+
+	c, err := client.NewHTTPClient(client.HTTPConfig{ // InfluxDB 연결
+		Addr: "http://" + INFLUX_IP + ":" + INFLUX_PORT, 
+		Username: INFLUX_USERNAME,
+		Password: INFLUX_PASSWORD,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{ // 배치 포인트 생성
+		Database:  INFLUXDB_NODE_DB, // DB 이름
+ 		Precision: "s",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var INFLUXDB_NODE_MEASUREMENT = "node" + strconv.Itoa(int(metricData.Id)) + "_metric" // csd 번호에 따라 테이블 설정 
+
+	// tags := map[string]string{"flags": "csd-metric"} //etcd 정보인지 csd 정보인지 분기 (etcd : 1, csd metric : 2)
+	fields := map[string]interface{}{
+		"current_time": time.Now().Format("15:04:05"),
+		"id": metricData.Id,
+
+		"node_name": metricData.NodeName,
+		"node_ip": metricData.NodeIp,
+		"node_status": metricData.NodeStatus,
+
+		"cpu_total": metricData.TotalCpuCapacity,
+		"cpu_usage": metricData.CpuUsage,
+
+		"memory_total":  metricData.TotalMemCapacity,
+		"memory_usage": metricData.MemUsage,
+		
+		"storage_total":  metricData.TotalStorageCapacity,
+		"storage_usage": metricData.StorageUsage,
+
+		"network_rx_byte": metricData.NetworkRxData,
+		"network_tx_byte": metricData.NetworkTxData,
+	}
+
+	pt, err := client.NewPoint(INFLUXDB_NODE_MEASUREMENT, nil, fields, time.Now()) //measurements에 tag, field, time insert => 필요 없는 값이면 nil로 설정, time은 nil 설정이 안됨
+	if err != nil {
+		log.Fatal(err)
+	}
+	bp.AddPoint(pt) // 생성해둔 배치 포인트에 새로운 데이터 추가 
+
+	err = c.Write(bp) //influxdb에 write
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // //80서버 instance metric collector grpc 서버 접속 및 메트릭 전송
@@ -269,9 +397,11 @@ func main() {
 
 	// stub handler 서버 등록 
 	pb.RegisterCSDMetricServer(s, &server{}) // register csd metric server
+	pb.RegisterNodeMetricServer(s, &server{}) // register node metric server
 	// pb.RegisterEtcdMetricServer(s, &server{}) // register etcd metric server start
     
 	fmt.Println("gRPC Server Created [port : 40801]")
+	
 	if err := s.Serve(lis); err != nil {
         log.Fatalf("failed to serve: %v", err)
     }
